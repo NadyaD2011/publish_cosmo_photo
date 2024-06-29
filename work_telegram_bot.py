d08e7d53ed9409ff5bdef5dc96bc5b47befbc9ae
NadyaEvp <t_evp@mail.ru>
2024-06-29 11:34:56 +0300

    publish text to Telegram channel.

--- work_telegram_bot.py
+++ work_telegram_bot.py
@@ -1,4 +1,5 @@
 import telegram
 
+chat_id = '@epic_photo_nasa_spacex'
 bot = telegram.Bot(token='7020625424:AAFbZ-VP81FKchx7yJezMZy7odYsSKvYYVg')
-print(bot)
+bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
