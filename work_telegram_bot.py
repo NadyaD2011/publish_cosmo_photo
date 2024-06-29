3ccb7b1dc226b909143d4b16e94c51ea86b306eb
NadyaEvp <t_evp@mail.ru>
2024-06-29 11:47:45 +0300

    publish photo to Telegram channel

--- work_telegram_bot.py
+++ work_telegram_bot.py
@@ -3,3 +3,4 @@ import telegram
 chat_id = '@epic_photo_nasa_spacex'
 bot = telegram.Bot(token='7020625424:AAFbZ-VP81FKchx7yJezMZy7odYsSKvYYVg')
 bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
+bot.send_document(chat_id=chat_id, document='https://gas-kvas.com/grafic/uploads/posts/2024-01/gas-kvas-com-p-chernii-kvadrat-oboi-s-nadpisyu-17.jpg')
