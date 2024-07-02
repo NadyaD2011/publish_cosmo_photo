6644ceb52e0ccee8784a490af4352c8a2c9bddac
NadyaEvp <t_evp@mail.ru>
2024-07-02 18:58:01 +0300

    Доработка отправки фотографий на сервер

--- work_telegram_bot.py
+++ work_telegram_bot.py
@@ -1,6 +1,35 @@
 import telegram
+import os
+import argparse
+import time
+import requests
+import random
 
-chat_id = '@epic_photo_nasa_spacex'
-bot = telegram.Bot(token='7020625424:AAFbZ-VP81FKchx7yJezMZy7odYsSKvYYVg')
-bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
-bot.send_document(chat_id=chat_id, document='https://gas-kvas.com/grafic/uploads/posts/2024-01/gas-kvas-com-p-chernii-kvadrat-oboi-s-nadpisyu-17.jpg')
+def open_photos(frequency, bot, chat_id):
+    while True:
+        for root, dirs, files in os.walk("images"):  
+            files = files
+
+        random.shuffle(files)
+        
+        for filename in files:
+            bot.send_document(chat_id=chat_id, document=open(f'images/{filename}', 'rb'))
+            time.sleep(frequency)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--frequency", help="Частота запусков", default='4')
+    args = parser.parse_args()
+    frequency = args.frequency
+    frequency = int(frequency)
+
+    chat_id = '@epic_photo_nasa_spacex'
+    bot = telegram.Bot(token='7020625424:AAFbZ-VP81FKchx7yJezMZy7odYsSKvYYVg')
+
+    try:
+        open_photos(frequency, bot, chat_id)
+    except requests.exceptions.HTTPError as error:
+        print("Can't get data from server:\n{0}".format(error))
+
+if __name__ == "__main__":
+    main()
