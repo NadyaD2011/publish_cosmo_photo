2c55fb9a05bf1b2573b81991df448d0cb6aeebc4
NadyaEvp <t_evp@mail.ru>
2024-07-10 21:05:56 +0300

    Переименование переменной

--- save_photos_nasa.py
+++ save_photos_nasa.py
@@ -1,20 +1,20 @@
 from dotenv import load_dotenv
-from auxiliary_code import save_files, create_folder
+from auxiliary_code import save_files
 import requests
 import os
 
 
-def save_photos_nasa(api_key, folder_name, name_foto):
+def save_photos_nasa(api_key):
     photo_urls = []
     url = f'https://api.nasa.gov/planetary/apod'
-    count_foto = 30
-    params = {'count': count_foto, "api_key": api_key}
+    quantity_foto = 30
+    params = {'count': quantity_foto, "api_key": api_key}
     response = requests.get(url, params=params)
     response.raise_for_status()
     response = response.json()
     for photo_url in response:
-        photo_urls.append(photo_url['url'])
-    save_files(folder_name, photo_urls, name_foto)    
+        photo_urls.append(photo_url['url'])   
+    return photo_url    
 
 
 def main():
@@ -23,8 +23,9 @@ def main():
     folder_name = 'images'
     name_foto = 'nasa_apod_'
 
-    create_folder(folder_name)
-    save_photos_nasa(api_key, folder_name, name_foto)
+    os.makedirs(folder_name, mode=0o777, exist_ok=True)
+    photo_urls = save_photos_nasa(api_key)
+    save_files(folder_name, photo_urls, name_foto) 
 
 
 if __name__ == '__main__':
