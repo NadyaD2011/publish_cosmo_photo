4bcea4270c95b6cf24bd51f3796fc3f1e94d10bf
NadyaEvp <t_evp@mail.ru>
2024-07-10 21:05:34 +0300

    Revert "Update save_photos_nasa.py"
    This reverts commit 91710edd5e9fa2c8bc9fdceebfd1920558362672.

--- save_photos_nasa.py
+++ save_photos_nasa.py
@@ -1,20 +1,20 @@
 from dotenv import load_dotenv
-from auxiliary_code import save_files
+from auxiliary_code import save_files, create_folder
 import requests
 import os
 
 
-def save_photos_nasa(api_key):
+def save_photos_nasa(api_key, folder_name, name_foto):
     photo_urls = []
     url = f'https://api.nasa.gov/planetary/apod'
-    quantity_foto = 30
-    params = {'count': quantity_foto, "api_key": api_key}
+    count_foto = 30
+    params = {'count': count_foto, "api_key": api_key}
     response = requests.get(url, params=params)
     response.raise_for_status()
     response = response.json()
     for photo_url in response:
-        photo_urls.append(photo_url['url'])   
-    return photo_url    
+        photo_urls.append(photo_url['url'])
+    save_files(folder_name, photo_urls, name_foto)    
 
 
 def main():
@@ -23,9 +23,8 @@ def main():
     folder_name = 'images'
     name_foto = 'nasa_apod_'
 
-    os.makedirs(folder_name, mode=0o777, exist_ok=True)
-    photo_urls = save_photos_nasa(api_key)
-    save_files(folder_name, photo_urls, name_foto) 
+    create_folder(folder_name)
+    save_photos_nasa(api_key, folder_name, name_foto)
 
 
 if __name__ == '__main__':
